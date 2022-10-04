838b472a75c14d382e6f9a9d4b48aa898b1ec714
Tore <ytterdorr@gmail.com>
2022-10-04 19:58:10 +0200

    TruckSizeSelect
    Select truck size: small, medium or large

--- src/components/InputsComponent.tsx
+++ src/components/InputsComponent.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import TruckSizeSelect from './TruckSizeSelect'
+import Inputs from '../models/Inputs';
 
 export default function InputsComponent() {
-  return (
+  const [truckSize, setTruckSize] = useState<Inputs["truckSize"]>('small')
+
+  return (<div>
     <div>Inputs</div>
+    <TruckSizeSelect onUpdate={(size) => {
+      console.log(size);
+      setTruckSize(size);
+    }} />
+    <div>Selected: {truckSize}</div>
+  </div>
   )
 }
---
+++ src/components/TruckSizeSelect/TruckSizeSelect.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import { Button } from 'react-bootstrap';
+import { propTypes } from 'react-bootstrap/esm/Image';
+import Inputs from "../../models/Inputs";
+import "./TruckSizeSelect.css"
+
+const sizes: [Inputs["truckSize"], Inputs["truckSize"], Inputs["truckSize"]] = ['small', 'medium', 'large']
+
+interface Props {
+    onUpdate: (truckSize: Inputs["truckSize"]) => void;
+}
+
+const TruckSizeSelect = ({ onUpdate }: Props) => {
+    const [selectedSize, setSelectedSize] = useState<Inputs["truckSize"]>('small');
+
+    const updateTruckSelect = (truckSize: Inputs["truckSize"]) => {
+
+        // const truckAlternatives = document.querySelector(".truckAlternative"))
+
+        setSelectedSize(truckSize);
+        onUpdate(truckSize)
+    }
+
+    return (
+        <div className="truck-size-container">
+            {sizes.map((size: Inputs["truckSize"]) => {
+                return (
+                    <div
+                        className={`truck-alternative ${selectedSize === size ? "selected" : null}`}
+                        onClick={() => updateTruckSelect(size)}
+                    ><div>
+
+                            {size} Truck
+                        </div>
+                    </div>
+                )
+            })}
+        </div >
+
+
+    );
+}
+
+export default TruckSizeSelect;
