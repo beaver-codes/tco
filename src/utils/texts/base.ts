12d5229ba210474212d9d27a0cb921efdccb3cb6
Pavel Schoffer <xschof00@gmail.com>
2022-10-05 20:24:12 +0200

    add texts

---
+++ src/utils/texts/base.ts
@@ -0,0 +1,16 @@
+const text = {
+    header: 'Total Cost of Ownership',
+    mainDescription: 'The absolutelly great things to come!',
+
+    personalCost: 'Personal cost',
+    serviceCost: 'Service cost',
+    energyCost: 'Energy cost',
+    overheadCost: 'Overhead cost',
+
+    bioGas: 'Biogas',
+    fossilDiesel: 'Fossil diesel',
+    fossilFree: 'Fossil free, HVO',
+    electric: 'Electric',
+};
+
+export default text;
